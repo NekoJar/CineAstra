2ced61d444b4f4254f6daea3cfd7444cb20018cf
Nekojar <mzharfanhakim7@gmail.com>
2023-08-12 11:25:35 +0700

    finishing the star component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating maxRating={5} color="green" onSetRating={setMovieRating} />
+      <p>pantek {movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+      maxRating={10}
+    />
+    <Test />
   </React.StrictMode>
 );
