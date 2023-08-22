909e5631f30582c3564fd169157e6dba4c59ad53
NekoJar <mzharfanhakim7@gmail.com>
2023-08-22 22:37:20 +0700

    fixing errors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
-
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating maxRating={5} color="green" onSetRating={setMovieRating} />
-      <p>pantek {movieRating}</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-      maxRating={10}
-    /> */}
-    {/* <Test /> */}
   </React.StrictMode>
 );
